41470b3bd3a0e476db7f76da96519f74e4130064
Cole Ramey <ceramey1997@gmail.com>
2021-08-09 01:50:55 -0500

    removed gh-repo name in Navbar.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,6 +15,7 @@ function Home() {
                 <p className="home-about-me">Some information about me piouqwehrksjdf;ashepandf;askdhfapseiuhaidhfa;skdhfasuidfhaksjdfhlkasdhflkajshdflkjahsdlfkjhasdlfkjhasdlfkjhasdfklh</p>
                 <p className="home-about-me">THIS IS TO TEST THE DEPLOYMENT</p>
                 <p className="home-about-me">another test, this one changed all paths to add gh repo name</p>
+                <p className="home-about-me">next next test... changed some other things</p>
             </div>
         </div>
     </div>
